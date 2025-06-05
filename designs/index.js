021d8214933ba6eefb2b45f5fb27958e5236a2ca
alaamer12 <ahmedmuhmmed239@gmail.com>
2025-06-05 15:33:51 +0300

    feat: update index.js with new design elements

---
+++ designs/index.js
@@ -0,0 +1,5 @@
+import AtomicComponents from "./AtomicComponents/AtomicComponents.jsx";
+import CompoundComponent from "./CompoundComponents/CompoundComponent.jsx";
+import ComponentInjection from "./ComponentInjection/ComponentInjection.jsx";
+
+export { AtomicComponents, CompoundComponent, ComponentInjection }
